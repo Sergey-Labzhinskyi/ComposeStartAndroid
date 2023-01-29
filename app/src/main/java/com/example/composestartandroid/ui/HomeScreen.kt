4806396a91b96bd453cb34327072c4517aa8bb78
Sergey-Labzhinskyi <labshinskiy@gmail.com>
2023-01-29 17:37:42 +0200

    Lesson 4

--- app/src/main/java/com/example/composestartandroid/ui/HomeScreen.kt
+++ app/src/main/java/com/example/composestartandroid/ui/HomeScreen.kt
@@ -1,9 +1,12 @@
 package com.example.composestartandroid.ui
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.BottomCenter
+import androidx.compose.ui.Alignment.Companion.Start
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
@@ -13,6 +16,21 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 internal fun HomeScreen() {
+    BoxLesson4()
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun HomeScreenPreview() {
+    HomeScreen()
+}
+
+/**
+ * Lesson 3
+ * ///////////////////////////////////////////////////////////////////////////////////////////////
+ */
+@Composable
+private fun Lesson3() {
     Text(
         text = "Home Screen",
         modifier = Modifier
@@ -26,8 +44,52 @@ internal fun HomeScreen() {
     )
 }
 
-@Preview(showBackground = true)
+/**
+ * Lesson 4
+ * /////////////////////////////////////////////////////////////////////////////////////////////////
+ */
 @Composable
-private fun HomeScreenPreview() {
-    HomeScreen()
+private fun ColumnLesson4() {
+    Column(
+        modifier = Modifier
+            .background(color = Color.Blue)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Bottom
+    ) {
+        Text(
+            text = "Title", fontSize = 32.sp
+        )
+        Text(
+            modifier = Modifier.align(Start), text = "Description", fontSize = 22.sp
+        )
+    }
+}
+
+@Composable
+private fun RowLesson4() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = "Name", fontSize = 20.sp)
+        Spacer(modifier = Modifier.weight(1f))
+        Text(text = "Surname", fontSize = 20.sp)
+
+    }
+}
+
+@Composable
+private fun BoxLesson4() {
+    Box {
+        Text(
+            text = "N", fontSize = 48.sp,
+            modifier = Modifier.background(color = Color.Green)
+        )
+        Text(
+            text = "ame",
+            modifier = Modifier
+                .background(color = Color.Yellow)
+                .align(BottomCenter)
+        )
+    }
 }
