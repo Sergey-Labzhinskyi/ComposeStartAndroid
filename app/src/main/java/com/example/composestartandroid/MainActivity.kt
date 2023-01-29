d6f70e44f41994514ee17e0135d782f217eece94
Sergey-Labzhinskyi <labshinskiy@gmail.com>
2023-01-29 16:22:30 +0200

    Lesson 3

--- app/src/main/java/com/example/composestartandroid/MainActivity.kt
+++ app/src/main/java/com/example/composestartandroid/MainActivity.kt
@@ -17,15 +17,17 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeStartAndroidTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    HomeScreen()
-                }
-            }
+            HomeScreen()
+
+            /* ComposeStartAndroidTheme {
+                 // A surface container using the 'background' color from the theme
+                 Surface(
+                     modifier = Modifier.fillMaxSize(),
+                     color = MaterialTheme.colorScheme.background
+                 ) {
+                     HomeScreen()
+                 }
+             }*/
         }
     }
 }
--- app/src/main/java/com/example/composestartandroid/ui/HomeScreen.kt
+++ app/src/main/java/com/example/composestartandroid/ui/HomeScreen.kt
@@ -1,17 +1,28 @@
 package com.example.composestartandroid.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 
 @Composable
 internal fun HomeScreen() {
     Text(
-        text = "HomeScreen",
-        fontSize = 22.sp,
+        text = "Home Screen",
+        modifier = Modifier
+            .background(color = Color.Red)
+            .fillMaxWidth(),
+        fontSize = 32.sp,
         color = Color.Green,
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis,
+        textAlign = TextAlign.Center
     )
 }
 
